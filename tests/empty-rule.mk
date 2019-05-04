382c51a4476ac29dcd8be81dd57b73940b2d0421
rfc <reuben.fletchercostin@gmail.com>
2019-05-04 19:53:06 +1000

    [cleanup] fix broken empty-rule.mk test
    This was an error in the test, it was failing for valid make behaviour.

--- tests/empty-rule.mk
+++ tests/empty-rule.mk
@@ -2,8 +2,20 @@
 	$(MAKE) -f $(word $(words $(MAKEFILE_LIST)),$(MAKEFILE_LIST)) obj
 
 obj: header
-	if [ -e obj ]; then echo TEST-FAIL; fi;
+
+	# Subtlety: make prints the source code of this shell snippet
+	# while executing it. But the test harness looks in the stdout
+	# from make for a certain special failure token to decide if
+	# the test has passed or failed. So we break the special token
+	# into two pieces, so it cannot be found by merely reading the
+	# source code without executing the source code.
+
+	# Regard test as failed if make attempts to build obj more than once.
+	if [ -e obj ]; then X=FAIL; echo TEST-$$X; fi;
 	touch obj
+
+	# Regard test as passed if make attempts to build obj at least once.
+	echo TEST-PASS
 
 header: cpp
 
